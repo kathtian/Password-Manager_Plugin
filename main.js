25dfbd2e0df7747c8106569e78888d86e818b3dc
kathtian <katherinetian@princeton.edu>
2023-12-14 12:01:49 -0500

    automatically fills in username password

--- main.js
+++ main.js
@@ -19,12 +19,6 @@ function onFormSubmit(event) {
     const website = window.location.href;
     console.log(usernameField, passwordField, website)
 
-    // var data = {
-    //     username: usernameField,
-    //     password: passwordField,
-    //     website: website
-    // }
-
     // if usernameField
     chrome.runtime.sendMessage({
         action: 'showPopup',
@@ -35,30 +29,35 @@ function onFormSubmit(event) {
 }
 
 // Detect password fields and add listeners
-document.querySelectorAll('input[type="password"]').forEach(inputField => {
-    inputField.addEventListener('change', async () => {
-      try {
-          const encryptionKey = await generateEncryptionKey();
+// document.querySelectorAll('input[type="password"]').forEach(inputField => {
+//     inputField.addEventListener('change', async () => {
+//       try {
+//           const encryptionKey = await generateEncryptionKey();
 
-          const { encryptedData, iv } = await encryptData(inputField.value, encryptionKey);
-          console.log('Encrypted data:', encryptedData);
+//           const { encryptedData, iv } = await encryptData(inputField.value, encryptionKey);
+//           console.log('Encrypted data:', encryptedData);
 
-          const decryptedData = await decryptData(encryptedData, encryptionKey, iv);
-          console.log('Decrypted data:', decryptedData);
-          // trigger the popup
-        //   chrome.runtime.sendMessage({
-        //       action: 'showPopup',
-        //       username: ,
-        //       password: inputfield,
-        //       website:
-        //   });
-      } catch (error) {
-          console.error('Error:', error);
-      }
-    });
-  });
+//           const decryptedData = await decryptData(encryptedData, encryptionKey, iv);
+//           console.log('Decrypted data:', decryptedData);
+//           // trigger the popup
+//         //   chrome.runtime.sendMessage({
+//         //       action: 'showPopup',
+//         //       username: ,
+//         //       password: inputfield,
+//         //       website:
+//         //   });
+//       } catch (error) {
+//           console.error('Error:', error);
+//       }
+//     });
+//   });
 
-  function getDataByWebsite(website) {
+document.addEventListener('DOMContentLoaded', () => {
+    const website = window.location.href;
+    getDataByWebsite(website);
+});
+
+function getDataByWebsite(website) {
     fetch(`http://localhost:5000/get?website=${encodeURIComponent(website)}`, {
         method: 'GET'
     })
@@ -66,11 +65,17 @@ document.querySelectorAll('input[type="password"]').forEach(inputField => {
         if (!response.ok) {
             throw new Error('No credentials found for this website');
         }
-        // return response.json();
+        return response.json();
     })
     .then(credentials => {
-        console.log('Credentials found:', credentials);
-        // You can now use the retrieved credentials as needed
+        // Automatically fill the username and password fields
+        const usernameField = document.querySelector('input[type="text"]');
+        const passwordField = document.querySelector('input[type="password"]');
+
+        if (usernameField && passwordField) {
+            usernameField.value = credentials.username;
+            passwordField.value = credentials.password;
+        }
     })
     .catch((error) => {
         console.error('Error:', error);
